a5bba7cfc016844f9f7cf45b288d9f2c1161c31c
Eric Onyango <eric.onyango@student.moringaschool.com>
2023-02-26 13:27:47 +0300

    Assignment Completed

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
   
-end
+    def characters_list
+      self.characters.map(&:name)
+    end
+  
+    def actors_list
+      self.actors.map(&:full_name)
+    end
+  end
